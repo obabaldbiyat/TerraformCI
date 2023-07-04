a1be3a3339fe0735f1994e476af486509fb68e41
obabaldbiyat <127845320+obabaldbiyat@users.noreply.github.com>
2023-07-04 16:10:04 +0200

    new

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
  
-  }
+  
 
 }
 
@@ -24,8 +24,8 @@ resource "random_string" "uniquestring" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "continuous-delivery-with-git"
-  location = "eastus"
+  name     = "obabgit"
+  location = "westeurope"
 }
 
 resource "azurerm_storage_account" "storageaccount" {
